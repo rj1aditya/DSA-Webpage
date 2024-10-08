fe80ef3a590338610db6ea8dd30be532531de7d8
rj1aditya <aditya.khurai@gmail.com>
2024-10-08 22:34:07 +0530

    new array problems added

--- topics/Array/file.cpp
+++ topics/Array/file.cpp
@@ -1,58 +1,29 @@
-#include &lt; bits / stdc++.h & gt;
+#include &lt; vector & gt;
 
-int longestSubarrayWithSumK(vector &lt; int &gt; a, long long k)
+vector & lt;
+vector & lt;
+int &gt;
+&gt;
+mergeOverlappingIntervals(vector &lt; vector & lt; int &gt; &gt; &arr)
 {
-    map & lt;
-    long long, int &gt;
-    m;
-    int maxi = INT_MIN;
-    int sum = 0;
-    for (int i = 0; i & lt; a.size(); i++)
-    {
-        sum += a[i];
-        if (sum == k)
-        {
-            maxi = max(maxi, i + 1);
-        }
+	// Write your code here.
+	vector & lt;
+	vector & lt;
+	int &gt;
+	&gt;
+	ans;
 
-        int leftNumber = sum - k;
-        if (m.find(leftNumber) != m.end())
-        {
-            maxi = max(maxi, i - m[leftNumber]);
-        }
-
-        if (m.find(sum) == m.end())
-        {
-            m[sum] = i;
-        }
-    }
-
-    return maxi == INT_MIN ? 0 : maxi;
-}
-
-// algo used: sliding window
-int longestSubarrayWithSumK1(vector &lt; int &gt; a, long long k)
-{
-    // Write your code here
-    int l = 0;
-    int r = 0;
-
-    long long sum = 0;
-    int maxi = INT_MIN;
-    while (r & lt; a.size())
-    {
-        sum += a[r];
-
-        while (sum & gt; k &&l &lt; = r)
-        {
-            sum -= a[l];
-            l++;
-        }
-        if (sum == k)
-        {
-            maxi = max(maxi, r - l + 1);
-        }
-        r++;
-    }
-    return maxi;
+	sort(arr.begin(), arr.end());
+	for (int i = 0; i & lt; arr.size(); i++)
+	{
+		if (ans.empty() || ans.back()[1] & lt; arr[i][0])
+		{
+			ans.push_back(arr[i]);
+		}
+		else
+		{
+			ans.back()[1] = max(ans.back()[1], arr[i][1]);
+		}
+	}
+	return ans;
 }
